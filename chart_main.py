301caf8af2cb610510da1f8293a97c30255cbc2f
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-19 21:16:36 +0900

    Update chart_main.py

--- chart_main.py
+++ chart_main.py
@@ -22,9 +22,9 @@ from notion.initialize_info_page import initialize_stock_page
 # 나중에는
 # make_chart 함수 하나, delete_chart 하나 update_chart 하나 이렇게 구성하는 것도 괜찮을 듯. 깔끔하게
 
-DOMESTIC_STOCK_CSV_PATH = Path("domestic_stock_info/csv/price_history.csv") 
-# 실제 데이터 경로 : Path("domestic_stock_info/csv/price_history.csv") 
-# 테스트용 경로 : Path("domestic_stock_info/csv/test_data.csv")
+DOMESTIC_STOCK_CSV_PATH = Path("data/csv/domestic_stock/price_history.csv") 
+# 실제 데이터 경로 : Path("data/csv/domestic_stock/price_history.csv") 
+# 테스트용 경로 : Path("data/csv/domestic_stock/test_data.csv")
 
 for page in get_all_pages(NOTION_DOMESTIC_STOCK_INFO_DB_ID):
     # 티커 데이터 추출
