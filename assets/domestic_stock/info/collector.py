4a3eeae08e9a935a477c39f699a772edab3bca8c
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-19 21:09:46 +0900

    Update collector.py

--- assets/domestic_stock/info/collector.py
+++ assets/domestic_stock/info/collector.py
@@ -1,6 +1,6 @@
 # naver_finance.py에서 함수를 호출해서 증권사 데이터 뽑아오기
 
-from collector.naver_finance import put_ticker_to_get_naver_prop
+from collectors.naver_finance import put_ticker_to_get_naver_prop
 
 def get_domestic_stock_info(ticker):
   properties = put_ticker_to_get_naver_prop(ticker)  # dictionary
