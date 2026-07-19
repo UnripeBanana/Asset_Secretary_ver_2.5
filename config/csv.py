137e40cc06f6e04812944da0ea45f954aaf7f31a
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-19 21:40:18 +0900

    Update csv.py

--- config/csv.py
+++ config/csv.py
@@ -2,3 +2,4 @@ from pathlib import Path
 
 DOMESTIC_STOCK_CSV_PATH = Path("data/csv/domestic_stock/price_history.csv")
 KRX_GOLD_CSV_PATH = Path("data/csv/krx_gold/price_history.csv")
+DOMESTIC_BOND_ETF_CSV_PATH = Path("data/csv/domestic_bond_etf/price_history.csv")
