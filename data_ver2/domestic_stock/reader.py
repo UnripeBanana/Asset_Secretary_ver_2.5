ea2bac80aa9f34a44c2a752684c4c375fd440584
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-20 19:03:12 +0900

    Update reader.py

--- data_ver2/domestic_stock/reader.py
+++ data_ver2/domestic_stock/reader.py
@@ -1,3 +1,32 @@
 #[['날짜', '시가', '고가', '저가', '종가', '거래량', '외국인소진율'],
 #["20240213", 74800, 75200, 74400, 75200, 21966745, 54.52]]
 #-> 이런 식으로 구성돼있음
+
+import requests
+import pandas as pd
+from io import StringIO
+
+start_day = 20240211
+end_day = 20260720
+
+start = f"&startTime={start_day}"
+end = f"&endTime={end_day}"
+
+url = (
+    "https://m.stock.naver.com/front-api/external/chart/domestic/info"
+    "?symbol=005930"
+    "&requestType=1"
+    start
+    end
+    "&timeframe=day"
+)
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+response = requests.get(url, headers=headers)
+
+df = pd.read_csv(StringIO(response.text))
+
+print(df.head())
