c1b1a61dc1d577cc2826136df59072f356393d60
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-20 21:00:03 +0900

    Update data_main.py

--- data_main.py
+++ data_main.py
@@ -2,6 +2,5 @@ from data_ver2.domestic_stock.reader import domestic_stock_data_reader
 from data_ver2.metal.gold_reader import gold_reader
 from data_ver2.make_market_chart import make_market_chart
 
-gold_reader()
 
-make_market_chart(df)
+make_market_chart(gold_reader())
