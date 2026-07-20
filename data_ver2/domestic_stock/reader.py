03da06a9cdf8dec57e6e9da5cd6534031685d6bb
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-20 19:07:05 +0900

    Update reader.py

--- data_ver2/domestic_stock/reader.py
+++ data_ver2/domestic_stock/reader.py
@@ -6,27 +6,28 @@ import requests
 import pandas as pd
 from io import StringIO
 
-start_day = 20240211
-end_day = 20260720
-
-start = f"&startTime={start_day}"
-end = f"&endTime={end_day}"
-
-url = (
-    "https://m.stock.naver.com/front-api/external/chart/domestic/info"
-    "?symbol=005930"
-    "&requestType=1"
-    start
-    end
-    "&timeframe=day"
-)
-
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}
-
-response = requests.get(url, headers=headers)
-
-df = pd.read_csv(StringIO(response.text))
-
-print(df.head())
+def domestic_stock_data_reader():
+    start_day = 20240211
+    end_day = 20260720
+    
+    start = f"&startTime={start_day}"
+    end = f"&endTime={end_day}"
+    
+    url = (
+        "https://m.stock.naver.com/front-api/external/chart/domestic/info"
+        "?symbol=005930"
+        "&requestType=1"
+        start
+        end
+        "&timeframe=day"
+    )
+    
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+    
+    response = requests.get(url, headers=headers)
+    
+    df = pd.read_csv(StringIO(response.text))
+    
+    print(df.head())
