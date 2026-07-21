1f4773012e6f4ec637b31f4896c6b1ed54352a59
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-21 20:54:54 +0900

    Update data_main.py

--- data_main.py
+++ data_main.py
@@ -8,29 +8,29 @@ from data_ver2.make_market_chart import make_market_index_chart
 #------------------------------
 # Metals
 #------------------------------
-make_market_index_chart(market_index_reader(20250721, 20260721, "metals", "M04020000", "KRX Gold"))
-make_market_index_chart(market_index_reader(20250721, 20260721, "metals", "GCcv1", "International Gold"))
-make_market_index_chart(market_index_reader(20250721, 20260721, "metals", "SIcv1", "International Silver"))
+make_market_index_chart(market_index_reader(20250721, 20260721, "metals", "M04020000", "KRX Gold", "KRW"))
+make_market_index_chart(market_index_reader(20250721, 20260721, "metals", "GCcv1", "International Gold", "USD"))
+make_market_index_chart(market_index_reader(20250721, 20260721, "metals", "SIcv1", "International Silver", "USD"))
 
 #------------------------------
 # Exchange
 #------------------------------
-make_market_index_chart(market_index_reader(20250721, 20260721, "exchange", "FX_USDKRW", "Dollar Won Exchange"))
-make_market_index_chart(market_index_reader(20250721, 20260721, "exchange", ".DXY", "Dollar Index"))
+make_market_index_chart(market_index_reader(20250721, 20260721, "exchange", "FX_USDKRW", "Dollar Won Exchange", "USD/KRW"))
+make_market_index_chart(market_index_reader(20250721, 20260721, "exchange", ".DXY", "Dollar Index", "Point"))
 
 #------------------------------
 # Energy
 #------------------------------
-make_market_index_chart(market_index_reader(20250721, 20260721, "energy", "CLcv1", "West Texas Intermediate"))
-make_market_index_chart(market_index_reader(20250721, 20260721, "energy", "LCOcv1", "Brent Crude"))
+make_market_index_chart(market_index_reader(20250721, 20260721, "energy", "CLcv1", "West Texas Intermediate", "USD"))
+make_market_index_chart(market_index_reader(20250721, 20260721, "energy", "LCOcv1", "Brent Crude", "USD"))
 
 #------------------------------
 # Bond
 #------------------------------
-make_market_index_chart(market_index_reader(20250721, 20260721, "bond", "US30YT%3DRR", "USA Bond 30y"))
-make_market_index_chart(market_index_reader(20250721, 20260721, "bond", "US10YT%3DRR", "USA Bond 10y"))
-make_market_index_chart(market_index_reader(20250721, 20260721, "bond", "US3YT%3DRR", "USA Bond 3y"))
+make_market_index_chart(market_index_reader(20250721, 20260721, "bond", "US30YT%3DRR", "USA Bond 30y", "USD"))
+make_market_index_chart(market_index_reader(20250721, 20260721, "bond", "US10YT%3DRR", "USA Bond 10y", "USD"))
+make_market_index_chart(market_index_reader(20250721, 20260721, "bond", "US3YT%3DRR", "USA Bond 3y", "USD"))
 
-make_market_index_chart(market_index_reader(20250721, 20260721, "bond", "KR30YT%3DRR", "Korea Bond 30y"))
-make_market_index_chart(market_index_reader(20250721, 20260721, "bond", "KR10YT%3DRR", "Korea Bond 10y"))
-make_market_index_chart(market_index_reader(20250721, 20260721, "bond", "KR3YT%3DRR", "Korea Bond 3y"))
+make_market_index_chart(market_index_reader(20250721, 20260721, "bond", "KR30YT%3DRR", "Korea Bond 30y", "KRW"))
+make_market_index_chart(market_index_reader(20250721, 20260721, "bond", "KR10YT%3DRR", "Korea Bond 10y", "KRW"))
+make_market_index_chart(market_index_reader(20250721, 20260721, "bond", "KR3YT%3DRR", "Korea Bond 3y", "KRW"))
