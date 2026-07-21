0d012b4c9073ac72dbe979406168a6ee05bc3094
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-21 16:50:51 +0900

    Update data_main.py

--- data_main.py
+++ data_main.py
@@ -4,4 +4,32 @@ from data_ver2.market_index.reader import market_index_reader
 #make_market_chart(gold_reader(20250721, 20260721, "M04020000", "KRX Gold"))
 #market_index_reader(start, end, category, ticker, name):
 
-make_market_chart(market_index_reader(20250721, 20260721, category, "M04020000", "KRX Gold"))
+#------------------------------
+# Metals
+#------------------------------
+make_market_chart(market_index_reader(20250721, 20260721, "metals", "M04020000", "KRX Gold"))
+make_market_chart(market_index_reader(20250721, 20260721, "metals", "GCcv1", "International Gold"))
+make_market_chart(market_index_reader(20250721, 20260721, "metals", "SIcv1", "International Silver"))
+
+#------------------------------
+# Exchange
+#------------------------------
+make_market_chart(market_index_reader(20250721, 20260721, "exchange", "FX_USDKRW", "Dollar/Won Exchange"))
+make_market_chart(market_index_reader(20250721, 20260721, "exchange", ".DXY", "Dollar Index"))
+
+#------------------------------
+# Energy
+#------------------------------
+make_market_chart(market_index_reader(20250721, 20260721, "energy", "CLcv1", "West Texas Intermediate"))
+make_market_chart(market_index_reader(20250721, 20260721, "energy", "LCOcv1", "Brent Crude"))
+
+#------------------------------
+# Bond
+#------------------------------
+make_market_chart(market_index_reader(20250721, 20260721, "bond", "US30YT%3DRR", "USA Bond 30y"))
+make_market_chart(market_index_reader(20250721, 20260721, "bond", "US10YT%3DRR", "USA Bond 10y"))
+make_market_chart(market_index_reader(20250721, 20260721, "bond", "US3YT%3DRR", "USA Bond 3y"))
+
+make_market_chart(market_index_reader(20250721, 20260721, "bond", "KR30YT%3DRR", "Korea Bond 30y"))
+make_market_chart(market_index_reader(20250721, 20260721, "bond", "KR10YT%3DRR", "Korea Bond 10y"))
+make_market_chart(market_index_reader(20250721, 20260721, "bond", "KR3YT%3DRR", "Korea Bond 3y"))
