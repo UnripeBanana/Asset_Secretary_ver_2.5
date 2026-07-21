99b7c98a7faf9bbee1ebdc29b5d73a794e0827ad
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-21 16:46:42 +0900

    Update market_index.py

--- config/market_index.py
+++ config/market_index.py
@@ -1,1 +1,51 @@
+금 차트 읽어오기 (page 조정 시 과거 데이터 볼 수 있음)
+https://m.stock.naver.com/front-api/marketIndex/prices?category=metals&reutersCode=M04020000&page=1
+"metals", "M04020000", "KRX Gold"
 
+달러/원 환율 읽어오기 (page 조정 시 과거 데이터 볼 수 있음)
+https://m.stock.naver.com/front-api/marketIndex/prices?category=exchange&reutersCode=FX_USDKRW&page=1
+"exchange", "FX_USDKRW", "Dollar/Won Exchange"
+
+국제 금
+https://m.stock.naver.com/front-api/marketIndex/prices?category=metals&reutersCode=GCcv1&page=1
+"metals", "GCcv1", "International Gold"
+
+국제 은
+https://m.stock.naver.com/front-api/marketIndex/prices?category=metals&reutersCode=SIcv1&page=1
+"metals", "SIcv1", "International Silver"
+
+WTI
+https://m.stock.naver.com/front-api/marketIndex/prices?category=energy&reutersCode=CLcv1&page=1
+"energy", "CLcv1", "West Texas Intermediate"
+
+브렌트유
+https://m.stock.naver.com/front-api/marketIndex/prices?category=energy&reutersCode=LCOcv1&page=1
+"energy", "LCOcv1", "Brent Crude"
+
+달러인덱스
+https://m.stock.naver.com/front-api/marketIndex/prices?category=exchange&reutersCode=.DXY&page=1
+"exchange", ".DXY", "Dollar Index"
+
+미국국채30년
+https://m.stock.naver.com/front-api/marketIndex/prices?category=bond&reutersCode=US30YT%3DRR&page=1
+"bond", "US30YT%3DRR", "USA Bond 30y"
+
+미국국채10년
+https://m.stock.naver.com/front-api/marketIndex/prices?category=bond&reutersCode=US10YT%3DRR&page=1
+"bond", "US10YT%3DRR", "USA Bond 10y"
+
+미국국채3년
+https://m.stock.naver.com/front-api/marketIndex/prices?category=bond&reutersCode=US3YT%3DRR&page=1
+"bond", "US3YT%3DRR", "USA Bond 3y"
+
+한국국채30년
+https://m.stock.naver.com/front-api/marketIndex/prices?category=bond&reutersCode=KR30YT%3DRR&page=1
+"bond", "KR30YT%3DRR", "Korea Bond 30y"
+
+한국국채10년
+https://m.stock.naver.com/front-api/marketIndex/prices?category=bond&reutersCode=KR10YT%3DRR&page=1
+"bond", "KR10YT%3DRR", "Korea Bond 10y"
+
+한국국채3년
+https://m.stock.naver.com/front-api/marketIndex/prices?category=bond&reutersCode=KR3YT%3DRR&page=1
+"bond", "KR3YT%3DRR", "Korea Bond 3y"
