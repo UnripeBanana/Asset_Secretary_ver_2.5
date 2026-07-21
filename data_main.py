cc974956e3b03d8183401f40598fab6fe2b3e95d
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-21 16:53:34 +0900

    Update data_main.py

--- data_main.py
+++ data_main.py
@@ -1,5 +1,6 @@
 from data_ver2.domestic_stock.reader import domestic_stock_data_reader
 from data_ver2.market_index.reader import market_index_reader
+from data_ver2.make_market_chart import make_market_chart
 
 #make_market_chart(gold_reader(20250721, 20260721, "M04020000", "KRX Gold"))
 #market_index_reader(start, end, category, ticker, name):
