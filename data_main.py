cd7136afc4b47d926e6872b5303c6feed95b9574
UnripeBanana <dlwjddnr0315@g.skku.edu>
2026-07-21 12:49:10 +0900

    Update data_main.py

--- data_main.py
+++ data_main.py
@@ -4,6 +4,11 @@ from data_ver2.make_market_chart import make_market_chart
 from data_ver2.exchange.exchange_reader import exchange_reader
 
 
-make_market_chart(gold_reader())
+make_market_chart(gold_reader(20250721, 20260721, "M04020000", "KRX Gold"))
+
+    #start = pd.to_datetime(start, format="%Y%m%d")
+    #end = pd.to_datetime(end, format="%Y%m%d")
+    #ticker = "M04020000"
+    #name = "KRX Gold"
 
 #make_market_chart(exchange_reader())
